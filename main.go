3096e570d65273c49376eac898d6ca45e71f7859
bananacode1234 <92938133+bananacode1234@users.noreply.github.com>
2023-12-01 06:53:33 -0500

    first

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	port := os.Getenv("PORT")
+
+	if strings.TrimSpace(port) == "" {
+		port = "3000"
+	}
+
+	app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+	app.Listen(":" + port)
+}
